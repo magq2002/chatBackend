16475ae4e3a06ab09cefcf105a0c50f02343125a
Billy Marles <131933489+Marles31@users.noreply.github.com>
2024-05-25 12:53:04 -0500

    Line spaces added

--- app/blueprints/messages/routes.py
+++ app/blueprints/messages/routes.py
@@ -8,19 +8,23 @@ from bson.objectid import ObjectId
 
 messages_bp = Blueprint('messages_bp', __name__)
 
+
 @messages_bp.route('/messages', methods=['POST'])
 @cross_origin(origins='http://localhost:4200')
 def saveMessage():
     return "hola"
+
 
 @messages_bp.route('/<message_id>', methods=['GET'])
 @cross_origin(origins='http://localhost:4200')
 def get_message(message_id):
     return "hola"
 
+
 @messages_bp.route('/', methods=['GET'])
 @cross_origin(origins='http://localhost:4200')
 def get_all_messages():
+
     messages = MessageModel.find_all()
     messages_data = [message.to_dict() for message in messages]
     return jsonify(messages_data)
--- app/services/conversion_service.py
+++ app/services/conversion_service.py
@@ -4,7 +4,9 @@ import tempfile
 import os
 from app.services.s3_service import upload_file_to_s3
 
+
 def convert_audio_to_text(audio_data):
+
     r = sr.Recognizer()
     with sr.AudioData(audio_data) as source:
         text = r.recognize_google(source, language='es-ES')
@@ -19,13 +21,15 @@ def convert_audio_to_text(audio_data):
 
     return (r.recognize_google(audio, language='es-Es'))
     '''
+
+
 def convert_text_to_audio(text):
     engine = pyttsx3.init()
 
     engine.setProperty('rate', 150)
 
     engine.setProperty('voice', 'spanish')
-    ##engine.say(text)
+    #engine.say(text)
     temp_audio_file = tempfile.NamedTemporaryFile(delete=False)
     temp_audio_path = temp_audio_file.name
     engine.save_to_file(text, temp_audio_path)
