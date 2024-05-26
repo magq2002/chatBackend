8118a6f2ad917a4e4bb68c7d800ff9833b5602bd
AsusMiguel <migalvez2002@gmail.com>
2024-05-25 22:34:21 -0500

    implementation audio to text

--- app/blueprints/voice_messages/routes.py
+++ app/blueprints/voice_messages/routes.py
@@ -1,20 +1,46 @@
 from flask import Blueprint, request, jsonify
+from flask_cors import cross_origin
 from app.models.message import MessageModel
 from app.services.s3_service import upload_file_to_s3
 from app.services.conversion_service import convert_audio_to_text
+import os
 
 voice_messages_bp = Blueprint('voice_messages_bp', __name__)
 
 
 @voice_messages_bp.route('/', methods=['POST'])
+@cross_origin(origins='http://localhost:4200')
 def post_voice_message():
     if 'audio' in request.files:
         audio = request.files['audio']
-        user = request.form.get('user_id')
-        #, text=convert_audio_to_text(audio)
-        new_message = MessageModel(name_audio=save_audio_to_s3(audio), is_audio=True, user=user)
+        user = int(request.form.get('user'))
+
+        audio_stream = audio.stream.read()
+        audio.stream.seek(0)
+
+        UPLOAD_FOLDER = 'static/uploads'
+        if not os.path.exists(UPLOAD_FOLDER):
+            os.makedirs(UPLOAD_FOLDER)
+        file_path = os.path.join(UPLOAD_FOLDER, audio.filename)
+        with open(file_path, 'wb') as f:
+            f.write(audio_stream)
+
+        try:
+            text=convert_audio_to_text(file_path)
+        except Exception as e:
+            os.remove(file_path)
+            text = 'No fui capaz de entenderte, podrias volver a intentarlo'
+            new_message = MessageModel(text=text, is_audio=False, user=2)
+            message_id = new_message.save()
+            return jsonify({'_id': message_id}), 200
+
+        os.remove(file_path)
+        name_audio = save_audio_to_s3(audio)
+
+
+        new_message = MessageModel(name_audio=name_audio, is_audio=True, user=user, text=text)
         message_id = new_message.save()
-        return jsonify({'message_id': message_id}), 200
+        return jsonify({'_id': message_id}), 200
     else:
         return jsonify({'error': 'No audio file found'}), 400
 
